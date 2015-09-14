10826f871b585c9d6aebfea220ca76e9397c9606
Tyler Riedal <triedal@mail.usf.edu>
2015-09-14 17:03:08 -0400

    Linked list reversal

---
+++ Linked Lists/reverseLL.c
@@ -0,0 +1,65 @@
+/*
+ *	Write code to reverse the nodes of a nexted list 
+ *  by manipulating pointer fields only. No new nodes must be 
+ *  created.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct node
+{
+	int value;
+	struct node *next;
+} Node;
+
+void printList(Node* head)
+{
+	while (head != NULL)
+  {
+		printf("%d ", head->value);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+void freeList(Node *head)
+{
+  while (head != NULL)
+  {
+    Node *victim = head;
+    head = head->next;
+    free(victim);
+  }
+}
+
+Node* reverseList(Node *first)
+{
+  if(first == NULL) return NULL;
+  if(first->next == NULL) return first;
+  Node* rest = reverseList(first->next);
+  first->next->next = first;
+  first->next = NULL;
+  return rest;
+}
+
+int main()
+{
+  Node *head = NULL, *tmp;
+    
+  srand((unsigned)time(NULL));
+    
+  for (int i=1; i<11; ++i)
+  {
+    tmp = malloc(sizeof(Node));
+    tmp->value = i;
+    tmp->next = head;
+    head = tmp;
+  }
+    
+  printList(head);
+  head = reverseList(head);
+  printList(head);
+  freeList(head);
+}
